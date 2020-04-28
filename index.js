895612fa31463f16aced86cdf4c8012ee277095b
Biktagirov Bulat <guzel28sit@gmail.com>
2020-04-28 13:29:54 +0300

    make it better"

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const bot = new TelegramBot("1187109592:AAFwLsqet3zl3DD7kHgGnOSJxtC9AbLwyM0", {
   webHook: {
-      port: process.env.PORT 
+      port: process.env.PORT || 8443
     } 
   });
-const url = process.env.APP_URL || 'https://designach.herokuapp.com:443';
+const url = 'https://designach.herokuapp.com:8443';
 
 bot.setWebHook(`${url}/bot`);
 // bot.on('new_chat_members', ctx => {
