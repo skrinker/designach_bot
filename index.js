6e3e1c00614b499f19207b3d3915850b26266c05
Biktagirov Bulat <guzel28sit@gmail.com>
2020-04-28 12:36:44 +0300

    make it better"

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ bot.on('new_chat_members', ctx => {
   ctx.reply("Привет! Показывай портфолио (если есть)");
 });
 
-bot.telegram.setWebhook(`${URL}`);
-bot.startWebhook(`${URL}`, null, 443);
+bot.telegram.setWebhook(`${URL}/`);
+bot.startWebhook(`${URL}/`, null, PORT);
