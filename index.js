0bab012badeebbc6f57097001a9385c65b81de6b
Biktagirov Bulat <guzel28sit@gmail.com>
2020-04-28 13:11:00 +0300

    make it better"

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const { Telegraf } = require('telegraf');
+const express = require('express')
+const app = express()
 
-const port = 443;
-const options = {
-  webHook: {
-    port: process.env.PORT
-  }
-};
-const bot = new Telegraf("1187109592:AAFwLsqet3zl3DD7kHgGnOSJxtC9AbLwyM0", options);
+const bot = new Telegraf("1187109592:AAFwLsqet3zl3DD7kHgGnOSJxtC9AbLwyM0");
 
-bot.telegram.setWebhook(`https://designach.herokuapp.com:443/bot`);
+app.use(bot.webhookCallback('/bot'));
+bot.telegram.setWebhook(`https://designach.herokuapp.com:8443/bot`);
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+})
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+})
 
 // bot.on('new_chat_members', ctx => {
 //   ctx.reply("Привет! Показывай портфолио (если есть)");
