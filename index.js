5f586d1dd7440d545252d4d2c0f8011f1050ab48
Biktagirov Bulat <guzel28sit@gmail.com>
2020-04-28 11:45:55 +0300

    make it better

---
+++ index.js
@@ -0,0 +1,8 @@
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf("1187109592:AAFwLsqet3zl3DD7kHgGnOSJxtC9AbLwyM0");
+
+bot.on('new_chat_members', ctx => {
+  ctx.reply("Привет! Показывай портфолио (если есть)");
+});
+bot.startPolling();
