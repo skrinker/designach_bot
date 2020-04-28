2e97da9c10cde996f89c015f5694357104722273
Biktagirov Bulat <guzel28sit@gmail.com>
2020-04-28 13:20:51 +0300

    make it better"

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const { Telegraf } = require('telegraf');
+const TelegramBot = require('node-telegram-bot-api');
 
-const bot = new Telegraf("1187109592:AAFwLsqet3zl3DD7kHgGnOSJxtC9AbLwyM0");
+const bot = new TelegramBot("1187109592:AAFwLsqet3zl3DD7kHgGnOSJxtC9AbLwyM0", {webHook: port: process.env.PORT});
+const url = process.env.APP_URL || 'https://designach.herokuapp.com:443';
 
 bot.telegram.setWebhook(`https://designach.herokuapp.com:8443/bot`);
 
-bot.startWebhook('bot', null, 8443);
+bot.startWebhook(`${url}/bot`);
 // bot.on('new_chat_members', ctx => {
 //   ctx.reply("Привет! Показывай портфолио (если есть)");
 // });
