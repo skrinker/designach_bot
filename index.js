52cf23546267f50a22512feb0c70b8a4e51b5e25
Biktagirov Bulat <guzel28sit@gmail.com>
2020-04-28 13:22:12 +0300

    make it better"

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const bot = new TelegramBot("1187109592:AAFwLsqet3zl3DD7kHgGnOSJxtC9AbLwyM0", {
   });
 const url = process.env.APP_URL || 'https://designach.herokuapp.com:443';
 
-bot.telegram.setWebhook(`https://designach.herokuapp.com:8443/bot`);
-
 bot.startWebhook(`${url}/bot`);
 // bot.on('new_chat_members', ctx => {
 //   ctx.reply("Привет! Показывай портфолио (если есть)");
