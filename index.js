645f82fb79f02d4b7a7c10ce39cf9dd07460c674
Biktagirov Bulat <guzel28sit@gmail.com>
2020-04-28 12:22:26 +0300

    make it better"

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Telegraf } = require('telegraf');
 
 
 const PORT = process.env.PORT || 3000;
-const URL = process.env.URL || 'https://designach.herokuapp.com/';
+const URL = 'https://designach.herokuapp.com/';
 
 const bot = new Telegraf("1187109592:AAFwLsqet3zl3DD7kHgGnOSJxtC9AbLwyM0");
 
@@ -10,5 +10,5 @@ bot.on('new_chat_members', ctx => {
   ctx.reply("Привет! Показывай портфолио (если есть)");
 });
 
-bot.telegram.setWebhook(`${URL}/bot`);
-bot.startWebhook(`${URL}/bot`, null, PORT);
+bot.telegram.setWebhook(`${URL}`);
+bot.startWebhook(`${URL}`, null, PORT);
