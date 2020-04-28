fdde3c34eff6b70a059e5357bf5c14d53dfc2281
Biktagirov Bulat <guzel28sit@gmail.com>
2020-04-28 12:54:23 +0300

    make it better"

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const bot = new Telegraf("1187109592:AAFwLsqet3zl3DD7kHgGnOSJxtC9AbLwyM0");
 
 bot.telegram.setWebhook('https://designach.herokuapp.com:8443/bot');
 
-bot.on('new_chat_members', ctx => {
-  ctx.reply("Привет! Показывай портфолио (если есть)");
-});
+// bot.on('new_chat_members', ctx => {
+//   ctx.reply("Привет! Показывай портфолио (если есть)");
+// });
+
+bot.action('message', ctx => ctx.reply("Fuk u"));
 
 bot.startWebhook('/bot', null, 8443);
