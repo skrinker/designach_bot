fd3a48aa8e49bae21b88189de1c11e6e1020a6cc
Biktagirov Bulat <guzel28sit@gmail.com>
2020-04-29 12:25:33 +0300

    make it better

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ const options = {
 const bot = new Telegraf(token, options);
 const url = process.env.APP_URL || 'https://designach.herokuapp.com:443';
 
-bot.setWebHook(`${url}/bot${token}`);
+bot.telegram.setWebHook(`${url}/bot${token}`);
 
+bot.startWebhook(`/bot${token}`);
 // bot.setWebHook(`${url}/bot`);
 // bot.on('new_chat_members', ctx => {
 //   ctx.reply("Привет! Показывай портфолио (если есть)");
